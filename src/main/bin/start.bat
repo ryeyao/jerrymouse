ec020429129fdcab9189d361fe51dc21776c50e8
Rye Yao <rye.y.cn@gmail.com>
2014-12-24 15:39:06 +0800

    Modified startup scripts. Should not use  and always specify  while start jerrymouse

--- src/main/bin/start.bat
+++ src/main/bin/start.bat
@@ -1,5 +1,5 @@
 cd %~dp0..\
 SET JERRY_HOME=%cd%
-rem java -cp lib/* -Dlog4j.configurationFile=conf/log4j2.xml -Dgaia.base=%JERRY_HOME% -Dgaia.home=%JERRY_HOME% org.omg.gaia.startup.Bootstrap
-java -Dlog4j.configurationFile=conf/log4j2.xml -Dgaia.base=%JERRY_HOME% -Dgaia.home=%JERRY_HOME% -jar lib/Jerrymouse-1.5.jar
+java -cp lib/* -Dlog4j.configurationFile=conf/log4j2.xml -Dgaia.base=%JERRY_HOME% -Dgaia.home=%JERRY_HOME% org.omg.gaia.startup.Bootstrap
+rem java -Dlog4j.configurationFile=conf/log4j2.xml -Dgaia.base=%JERRY_HOME% -Dgaia.home=%JERRY_HOME% -jar lib/Jerrymouse-1.5.jar
 pause
--- src/main/bin/start.sh
+++ src/main/bin/start.sh
@@ -2,4 +2,4 @@
 
 cd $(dirname $0)/..
 JERRY_HOME=$(pwd)
-java -Dlog4j.configurationFile=conf/log4j2.xml -Dgaia.base=${JERRY_HOME} -Dgaia.home=${JERRY_HOME} -jar lib/Jerrymouse-1.5.jar
+java -cp "lib/*" -Dlog4j.configurationFile=conf/log4j2.xml -Dgaia.base=${JERRY_HOME} -Dgaia.home=${JERRY_HOME} org.omg.gaia.startup.Bootstrap
